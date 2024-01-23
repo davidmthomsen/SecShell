6e63aa3cce1bc429151f3f3b6ca508bc7e342003
davidmthomsen <davidmthomsen@gmail.com>
2024-01-22 19:50:51 -0500

    Adding functions

--- cli.py
+++ cli.py
@@ -1,11 +1,24 @@
 import argparse
+import base64
+import hashlib
+
 
 def encode_data(data, encoding_type):
     # Function to handle data encoding
-    pass
+    if encoding_type == 'base64':
+        encode_data = base64.b64encode(data.encode('utf-8')) # Encode strings to bytes
+        print(encode_data.decode('utf-8')) # Decode bytes to string for display 
+    else:
+        print(f"Encoding type {encoding_type} is not implemented.")
 
 def hash_data(data, hash_type):
     # Function to handle data hashing 
+    if hash_type == 'sha256':
+        hash_object = hashlib.sha256(data.encode('utf-8')) # Hashing requires encode bytes
+        hex_dig = hash_object.hexdigest() # Get hexadecimal digest string
+        print(hex_dig)
+    else:
+        print(f"Hash type {hash_type} not implemented.")
     pass
 
 def main():
@@ -14,8 +27,8 @@ def main():
 
     # Subparser for encoding
     encode_parser = subparsers.add_parser("encode")
-    encode_parser.add_argument("type", choices=["base64", "hex", "url"])
-    encode_parser.add_argument("data")
+    encode_parser.add_argument('data', type=str, help='Data to encode')
+    encode_parser.add_argument('type', type=str, choices=['base64'], help='Type of encoding.')
     encode_parser.set_defaults(func=encode_data)
 
     # Subarser for hashing 
@@ -33,4 +46,9 @@ def main():
         parser.print_help()
 
 if __name__ == "__main__":
+    args = parser.parse_args()
+    if 'func' in args:
+        args.func(args.data, args.type) # Call function passed by set_defaults
+    else:
+        parser.print_help()
     main()
